f30c26903ec1ff205ed928e24290ad03535e6a52
Mickael <mickael.spilmont.fr@gmail.com>
2018-09-13 12:04:05 +0200

    Completion de la classe Magasin

--- src/hypermarche/Magasin.java
+++ src/hypermarche/Magasin.java
@@ -10,4 +10,14 @@ public class Magasin {
 		this.journal = journal;
 		listeClients = new ArrayList<>();
 	}
+	
+	public void ajouterClient(Client client) {
+		listeClients.add(client);
+	}
+	
+	public boolean retirerClient(Client client) {
+		return listeClients.remove(client);
+	}
+	
+//	public int effect
 }
